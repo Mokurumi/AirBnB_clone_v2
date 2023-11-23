26828848c2247ee162be3dce4950320b0cb8c0b1
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-11-23 05:43:49 +0300

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,17 @@
--- Creates the database hbnb_dev_db with specified parameters
--- Create database
+-- Create the database hbnb_dev_db with specified parameters
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Creates user if doesn't exist and sets password
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 
--- Grants privileges to user on database and performance_schema
+-- Set a password for the user
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+
+-- Grant privileges to the user on the database
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant select privileges to the user on the performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
--- Flush privileges to apply the changes
+-- Flush privileges
 FLUSH PRIVILEGES;
