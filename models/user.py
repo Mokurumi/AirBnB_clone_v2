9f3bdfa045d05ea7347a7107376c8d6ba83e0462
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2023-11-23 05:31:45 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""This is the user class"""
+"""User class for storing user information."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
-    """This is the class for user
+    """User class for storing user information.
+
     Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+        email (str): Email address.
+        password (str): Password for login.
+        first_name (str): First name.
+        last_name (str): Last name.
     """
     __tablename__ = "users"
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
     places = relationship(
         "Place",
         cascade="all,delete",
