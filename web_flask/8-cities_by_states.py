5ec30028219de1932ea4d4f48b75f17a663d90c7
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-01-04 01:48:15 +0300

    Create 8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This script that starts a Flask web application
+
+Write a script that starts a Flask web application:
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all State objects in DBStorage"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
